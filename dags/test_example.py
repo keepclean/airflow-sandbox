c1fad08388bac06ed0d7995ccc3bce1deda31503
Rustam Iuldashev <rustam.iuldashev@protonmail.com>
2019-12-06 22:22:22 +0000

    black formats python code... ah, robots...

--- dags/test_example.py
+++ dags/test_example.py
@@ -6,25 +6,23 @@ from airflow.operators.python_operator import PythonOperator
 import time
 
 args = {
-    'owner': 'airflow',
-    'start_date': airflow.utils.dates.days_ago(1),
+    "owner": "airflow",
+    "start_date": airflow.utils.dates.days_ago(1),
 }
 
 dag = DAG(
-    dag_id='test_example_python_operator',
-    default_args=args,
-    schedule_interval=None,
+    dag_id="test_example_python_operator", default_args=args, schedule_interval=None,
 )
 
 
 def print_context(ds, **kwargs):
     print(kwargs)
     print(ds)
-    return 'this line should be in the logs'
+    return "this line should be in the logs"
 
 
 run_this = PythonOperator(
-    task_id='print_the_context',
+    task_id="print_the_context",
     provide_context=True,
     python_callable=print_context,
     dag=dag,
@@ -37,9 +35,9 @@ def sleep(t):
 
 for i in range(5):
     task = PythonOperator(
-        task_id='sleep_for_{}'.format(i),
+        task_id="sleep_for_{}".format(i),
         python_callable=sleep,
-        op_kwargs={'t': float(i) / 10},
+        op_kwargs={"t": float(i) / 10},
         dag=dag,
     )
 
